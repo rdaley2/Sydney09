26ef0b568f783cb24b1f0ddd9c250ebe2df503ed
Ben Ackerman <backerm3@u.rochester.edu>
2017-12-07 20:55:03 -0500

    The view page works!

--- OTHER TRIAL SORRY GUYS HAHA/Lab09/admin/view.php
+++ OTHER TRIAL SORRY GUYS HAHA/Lab09/admin/view.php
@@ -1,160 +1,77 @@
 <?php
 
-	$dbhost = "66.147.242.186";
-	$dbuser = "urcscon3_sydney";
-	$dbpass = "coffee1N";
-	$dbname = "urcscon3_sydney";
+require "../includes/checkAuth.php";
+require "../includes/database.php";
 
-	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-	$counter = $_POST['counter'];
-
-	$query  = "SELECT * ";
-	$query .= "FROM survey ";
-	$query .= "WHERE counter = {$counter} ";
-	$query .= "LIMIT 1";
-
-	$result = $connection->query($query);
-
-
+$result = mysqli_query($db, "SELECT * FROM survey9;");
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title>Administration Page</title>
+		<title>Sydney Administration System</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="css/styles.css">
-		<link rel="stylesheet" href="css/grid.css">
+		<link rel="stylesheet" href="../css/styles.css">
+		<link rel="stylesheet" href="../css/grid.css">
+		<link rel="stylesheet" href="../css/nav.css">
+		
+		<script type="text/javascript">
+			function deleteRow(id) {
+				if ( confirm('This response will be DELETED!  Is this really what you want?') ) {
+					window.location.href='delete.php?id=' + id;
+				}
+			}
+		</script>
 	</head>
 
-
-<?php
-	// 3. Use returned data (if any)
-	while($row = mysqli_fetch_assoc($result)) {
-		// output data from each row
-?>
-
 	<body>
-		<h1>Administration Update Page</h1>
+		<div class="topnav" id="myTopnav">
+		  <span>Logged in as <?php echo $_SESSION["user"]; ?></span>
+		  <a href="logout.php">Log Out</a>
+		</div>
+		
 		<div class="container">
+			<h1>Survey Results</h1>
 			
-			<h2>Updating Account # <?php echo $row['counter'] ?>, (<?php echo $row['fname'] ?> <?php echo $row['lname'] ?>)</h2>
-			<div class="formformat">
-				<h2>Original Survey Results</h2>
-					<table border>
-						<tr>
-							<td> ID# </td>
-							<td><?php echo $row["counter"]; ?></td>
-						</tr>
-
-						<tr>
-							<td>Full Name</td>
-							<td><?php echo $row["fname"]; ?> <?php echo $row["lname"]; ?></td>
-						</tr>
-
-						<tr>
-							
-							
-							<td>Email</td>
-							<td><?php echo $row["email"]; ?></td>
-							
-							
-						</tr>
-
-						<tr>
-							<td> Phone# </td>
-							<td><?php echo $row["pnumber"]; ?></td>
-						</tr>
-
-						<tr>
-							<td>Which Candies Do You Like?</td>
-						</tr>
-						<tr>
-							<td><?php echo $row["like_kit"]; ?></td>
-							<td><?php echo $row["like_poc"]; ?></td>
-						</tr>
-						<tr>
-							<td>Which One Do You Like The Most?</td>
-						</tr>
-						<tr>
-							<td><?php echo $row["fav"]; ?></td>
-						</tr>
-						<tr>
-							<td>Why did you choose the answer above?</td>
-						</tr>
-						<tr>
-							<td><?php echo $row["message"]; ?></td>
-						</tr>
-					</table>
-<?php } ?>	
-		<div class="mainbox2">
-		   		<ul class="formformat">
-		   			<li>
-					   <form method="post" action="admin.php">
-					   	<input type="submit" value="Back to Admin Page" />
-					   </form>
-					</li>
-				</ul>
-			</div>
-	   </div>
-					<div class="mainbox">	
-	
-						<h2>Fill Out The Entire Survey To Update The Data.</h2>
-
-						<form method="post" action="update.php">
-					<ul class="formformat">
-					    <li>
-					    	<label>Full Name </label>
-					    	<input type="text" name="fname" id="fname" class="field-divided" placeholder="First" required/>&nbsp;<input type="text" name="lname" id="lname" class="field-divided" placeholder="Last" required/>
-					    </li>
-					    <li>
-					        <label>Email</label>
-					        <input type="email" name="email" class="field-long" required/>
-					    </li>
-					    <li>
-					    	<label>Phone Number</label>
-					        <input type="number" name="pnumber" class="field-long" />
-					    </li>
-					    <li>
-					    	<label>Which Candies Do You Like?</label>
-					    	<input type="checkbox" name="pocky" id="kitkat" value="pocky"> Pocky<br>
-							<input type="checkbox" name="kitkat" id="kitkat" value="kitkat"> Kitkat<br>
-					    </li>
-
-					    <li>
-					    	<label>Which One Do You Like The Most?</label>
-					    	<input type="radio" name="preference" value="pocky2"> Pocky<br>
-							<input type="radio" name="preference" value="kitkat2"> Kitkat<br>
-							<input type="radio" name="preference" value="cantchoose"> I can't choose<br>
-							<input type="radio" name="preference" value="neither"> Neither
-					    </li>
-
-					    <li>
-					        <label>Why did you choose the answer above?</label>
-					        <textarea name="message" class="field-long field-textarea"></textarea>
-					    </li>
-					    <li>
-					        <button type="submit" name="counter" value=<?php echo $counter; ?>>Resubmit Data</button> 
-					    </li>
-					</ul>
-					</form>
-					</div>
-
+			<table>
+				<tr>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Phone</th>
+					<th>Familiar With...</th>
+					<th>Tried...</th>
+					<th>Prefers...</th>
+					<th>Because...</th>
+					<th>Admin Tasks</th>
+				</tr>
 				
-
-			</div>	
+				<?php
+				
+				while ( $row = mysqli_fetch_assoc($result) ) {
+					echo "<tr>";
+					
+					echo "<td>".$row["firstName"]." ".$row["lastName"]."</td>";
+					echo "<td>".$row["email"]."</td>";
+					echo "<td>".$row["cellNum"]."</td>";
+					echo "<td>".$row["familiar"]."</td>";
+					echo "<td>".$row["tried"]."</td>";
+					echo "<td>".$row["prefer"]."</td>";
+					echo "<td>".$row["why"]."</td>";
+					echo "<td><a href=\"edit.php?id=".$row["ID"]."\">Edit</a>&nbsp;|&nbsp;<a href=\"javascript:deleteRow(".$row["ID"].");\">Delete</a></td>";
+					
+					echo "</tr>";
+				}
+				
+				?>
+				
+			</table>
 		</div>
 	</body>
 </html>
 
 <?php
-	// 4. Release returned data
-	mysqli_free_result($result);
-
-	// 5. Close database connection
-	mysqli_close($connection);
+mysqli_free_result($result);
+mysqli_close($db);
 ?>
