406b51e8f07c6f6b4818f3e8e80b57284fb0cd29
JackiePowell <31747169+JackiePowell@users.noreply.github.com>
2017-12-08 21:57:09 -0500

    getting rid of contact

--- OTHER TRIAL SORRY GUYS HAHA/Lab09/admin/login.php
+++ OTHER TRIAL SORRY GUYS HAHA/Lab09/admin/login.php
@@ -23,7 +23,6 @@ if ( isset($_SESSION["user"]) ) {
 	<body>
 		<div class="topnav" id="myTopnav">
 		  <a href="../assignment09.html">Home</a>
-		  <a href="#">Contact</a>
 		  <a href="admin/login.php">Admin</a>
 		</div>
 	   
